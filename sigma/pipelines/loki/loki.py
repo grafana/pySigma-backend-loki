75fa9db5e755d9dcf19b6c0dd128f1c360a3836d
Nick Moore <nicholas.moore@grafana.com>
2022-12-04 22:09:45 +0000

    Implement sysmon parsing and overriding log parser
    Windows sysmon data of standard promtail output requires significantly
    more parsing than just simply using json. To achieve this, the queries
    need additional code (including calling a second logfmt parser).
    
    This adds in this functionality, as a separate pipeline, along with the
    code to allow custom parsers to be specified in a pipeline.

--- sigma/pipelines/loki/loki.py
+++ sigma/pipelines/loki/loki.py
@@ -1,5 +1,20 @@
+from dataclasses import dataclass
+from sigma.rule import SigmaRule
+from sigma.processing.conditions import LogsourceCondition
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
-from sigma.processing.transformations import FieldMappingTransformation
+from sigma.processing.transformations import Transformation, FieldMappingTransformation
+
+
+@dataclass
+class SetLokiParserTransformation(Transformation):
+    """Sets the relevant parser for the log data in the custom_attributes for an
+    applicable rule."""
+
+    parser: str
+
+    def apply(self, pipeline: ProcessingPipeline, rule: SigmaRule) -> None:
+        super().apply(pipeline, rule)
+        rule.custom_attributes["loki_parser"] = self.parser
 
 
 def loki_grafana_logfmt() -> ProcessingPipeline:
@@ -21,6 +36,30 @@ def loki_grafana_logfmt() -> ProcessingPipeline:
                         "sc-status": "status",
                     }
                 ),
+            )
+        ],
+    )
+
+
+def loki_windows_sysmon_message_parser() -> ProcessingPipeline:
+    return ProcessingPipeline(
+        name="Loki Windows Sysmon Message Parser",
+        priority=20,
+        items=[
+            ProcessingItem(
+                identifier="loki_sysmon_message_parsing",
+                transformation=SetLokiParserTransformation(
+                    "json"
+                    '| label_format Message=`{{ .message | replace "\\" "\\\\" | replace """ "\\"" }}`'  # noqa: E501
+                    '| line_format `{{ regexReplaceAll "([^:]+): ?((?:[^\\r]*|$))(\r\n|$)" .Message "${1}="${2}" "}}`'  # noqa: E501
+                    "| logfmt"
+                ),
+                rule_conditions=[
+                    LogsourceCondition(
+                        product="windows",
+                        service="sysmon",
+                    )
+                ],
             )
         ],
     )
