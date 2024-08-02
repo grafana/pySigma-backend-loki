38196827da14923bf1907a8cd9bf124ade15c3aa
Nick Moore <nicholas.moore@grafana.com>
2024-08-02 15:40:13 +0100

    Add unit test for Okta conversion
    Will currently fail, as the LogsourceCondition in the Okta pipeline
    prevents the transformations from being applied to the Correlation rule.
    I've raised this with the pySigma maintainers in Discord.

--- tests/test_backend_loki_value_count_correlation.py
+++ tests/test_backend_loki_value_count_correlation.py
@@ -3,6 +3,8 @@ import pytest
 from sigma.backends.loki import LogQLBackend
 from sigma.collection import SigmaCollection
 
+from sigma.pipelines.loki import loki_okta_system_log
+
 
 @pytest.fixture
 def loki_backend():
@@ -73,7 +75,7 @@ correlation:
                        '{job=~".+"} | logfmt | fieldA=~`(?i)^valueA$` [5m]))) >= 1']
 
 
-def test_loki_default_event_count_multiple_fields(loki_backend: LogQLBackend):
+def test_loki_default_value_count_multiple_fields(loki_backend: LogQLBackend):
     rules = SigmaCollection.from_yaml(
         """
 title: Test Rule
@@ -105,3 +107,55 @@ correlation:
     queries = loki_backend.convert(rules)
     assert queries == ['count without (fieldD) (sum by (fieldB, fieldC, fieldD) (count_over_time('
                        '{job=~".+"} | logfmt | fieldA=~`(?i)^valueA$` [1d]))) < 100']
+
+
+def test_loki_okta_country_count():
+    pipeline = loki_okta_system_log()
+    rules = SigmaCollection.from_yaml(
+        """
+title: Okta User Activity With Country Defined
+id: 79bbc335-7ab0-4316-a17b-30c85f7f0595
+status: experimental
+description: Detects any Okta activity that includes a country
+references:
+    - https://developer.okta.com/docs/reference/api/system-log/
+author: kelnage
+date: 2024-08-01
+logsource:
+    product: okta
+    service: okta
+detection:
+    selection:
+        actor.alternateid|exists: true
+        client.geographicalcontext.country|exists: true
+    condition: selection
+falsepositives:
+    - If a user requires an anonymising proxy due to valid justifications.
+level: high
+---
+title: Okta User Activity Across Multiple Countries
+id: a8c75573-8513-40c6-85a6-818b7c58a601
+author: kelnage
+date: 2024-08-01
+status: experimental
+correlation:
+    type: value_count
+    rules:
+        - 79bbc335-7ab0-4316-a17b-30c85f7f0595
+    group-by:
+        - actor.alternateid
+    timespan: 1h
+    condition:
+        field: client.geographicalcontext.country
+        gt: 1
+level: high
+"""
+    )
+    loki_backend = LogQLBackend(processing_pipeline=pipeline)
+    queries = loki_backend.convert(rules)
+    assert queries == ['count without (event_client_geographicalContext_country) '
+                       '(sum by (event_actor_alternateId, '
+                       'event_client_geographicalContext_country) '
+                       '(count_over_time({job=~".+"} | json | event_actor_alternateId!="" and '
+                       'event_client_geographicalContext_country!="" [1h]))) '
+                       '> 1']
